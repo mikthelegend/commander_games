cafde60ab4e1e0d6b339ce07b417efee39ca33d4
Mikail Yildirim <z5360510@ad.unsw.edu.au>
2025-09-03 15:37:24 +1000

    Tidy up unused code

--- manual.py
+++ manual.py
@@ -2,8 +2,3 @@ import main
 
 main.calculate_elos()
 main.update_spreadsheet()
-
-# for deck in main.all_decks:
-#     print(f"{deck['deck_name']}:")
-#     for elo in deck['elos']:
-#         print(f"  Elo: {elo['elo']}, Date: {elo['date']}")
