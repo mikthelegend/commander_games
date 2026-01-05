dfba344d0bf2fa164b8ee988917ad70afd28eddb
Mikail Yildirim <mymikystar@gmail.com>
2026-01-05 14:59:18 +1100

    add datastore functionality and save a backup

--- manual.py
+++ manual.py
@@ -1,3 +1,11 @@
 import main
+import util
 
 main.update_spreadsheet()
+
+data = {
+    "decks": [deck.json() for deck in main.all_decks],
+    "games": [game.json() for game in main.all_games]
+}
+
+util.save_data(data)
---
+++ util.py
@@ -0,0 +1,12 @@
+import json
+
+def save_data(data):
+    with open('data.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
+def load_data():
+    try:
+        with open('data.json', 'r') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
