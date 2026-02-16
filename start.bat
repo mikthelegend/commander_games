9331386832c3aa73131372865757dd6269cbce85
Mikail Yildirim <mymikystar@gmail.com>
2026-02-16 14:55:42 +1100

    Create start.bat to run the server more simply

---
+++ start.bat
@@ -0,0 +1,14 @@
+rem Step 1: Activate Miniconda, use the actual path where your Miniconda/Ananconda is installed.
+call "E:\miniconda3\Scripts\activate.bat" "E:\miniconda3\"
+
+rem Step 2: Activate Conda environment. 
+call conda activate commander_games
+
+rem Step 3: Change directory to the desired folder.
+cd /d "E:\webservers\commander_games"
+
+rem Step 4: Run the Python script.
+flask run --host=0.0.0.0
+
+rem Step 5: Keep the command prompt open after execution (optional)
+cmd /k
