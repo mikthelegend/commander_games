4602d3b0169d1856e18e27d4803a72bb6aa88fc8
Bodhi Tuladhar <bodhituladhar@gmail.com>
2026-02-17 00:40:23 +1100

    raise buttons and cards

--- static/util.js
+++ static/util.js
@@ -1,12 +1,14 @@
 function generateGameCard(game) {
     return `
-        <b>Date:</b> ${game.date} &emsp;&emsp;&emsp;&emsp;&ensp; <b>Game ID:</b> ${game.game_id} <br>
+        <b>Date:</b> ${game.date} &emsp;&emsp;&emsp;&emsp; <b>Game ID:</b> ${game.game_id} <br>
+        <br>
         <br>
         <b>Winner:</b> ${game.winning_player} <br>
         ${game.winning_deck.name} [${(game.winning_deck.elo_after - game.winning_deck.elo_before < 0 ? "" : "+") + Math.round(game.winning_deck.elo_after - game.winning_deck.elo_before)}] <br>
         <br>
         <b>Losers:</b> ${game.losing_players.join(", ")} <br>
         ${game.losing_decks.map(deck => `${deck.name} [${(deck.elo_after - deck.elo_before < 0 ? "" : "+") + Math.round(deck.elo_after - deck.elo_before)}]`).join("<br>")} <br>
+        <br>
         <br>
         <b>Notes:</b> <br>
         ${game.notes}
