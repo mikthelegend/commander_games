5a0b8d48ade7b9afe4b17519f8e9aa7ca24d921d
Bodhi Tuladhar <bodhituladhar@gmail.com>
2026-02-17 15:09:27 +1100

    add card header/footer

--- static/util.js
+++ static/util.js
@@ -1,16 +1,35 @@
 function generateGameCard(game) {
+    const split = game.date.split("/");
+    const date = new Date('20' + split[2], split[1], split[0])
+    const options = {
+        weekday: "short",
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+    }
     return `
-        <b>Date:</b> ${game.date} &emsp;&emsp;&emsp;&emsp; <b>Game ID:</b> ${game.game_id} <br>
-        <br>
-        <br>
-        <b>Winner:</b> ${game.winning_player} <br>
-        ${game.winning_deck.name} [${(game.winning_deck.elo_after - game.winning_deck.elo_before < 0 ? "" : "+") + Math.round(game.winning_deck.elo_after - game.winning_deck.elo_before)}] <br>
-        <br>
-        <b>Losers:</b> ${game.losing_players.join(", ")} <br>
-        ${game.losing_decks.map(deck => `${deck.name} [${(deck.elo_after - deck.elo_before < 0 ? "" : "+") + Math.round(deck.elo_after - deck.elo_before)}]`).join("<br>")} <br>
-        <br>
-        <br>
-        <b>Notes:</b> <br>
-        ${game.notes}
+            <div id=game_card_header>
+                <div>
+                    ${date.toLocaleDateString("en-AU", options)}
+                </div>
+                <div>
+                    <b>ID</b> ${game.game_id}
+                </div>
+            </div>
+            <div id=game_card_body>
+                <div>
+                    <div><b>Winner:</b> ${game.winning_player}</div>
+                    <div>${game.winning_deck.name} [${(game.winning_deck.elo_after - game.winning_deck.elo_before < 0 ? "" : "+") + Math.round(game.winning_deck.elo_after - game.winning_deck.elo_before)}]</div>
+                </div>
+                <br>
+                <div>
+                    <div><b>Losers:</b> ${game.losing_players.join(", ")}</div>
+                    ${game.losing_decks.map(deck => `${deck.name} [${(deck.elo_after - deck.elo_before < 0 ? "" : "+") + Math.round(deck.elo_after - deck.elo_before)}]`).join("<br>")} <br>
+                </div>
+            </div>
+            <div id=game_card_footer> 
+                <!--<b>Notes:</b> <br>-->
+                ${game.notes}
+            </div>
         `;
 }
